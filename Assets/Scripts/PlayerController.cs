dd94f58de3799cef04ca5465e6a7f867086d8006
RogerMartin-pero <59997775+RogerMartin-pero@users.noreply.github.com>
2020-04-13 18:43:06 +0200

    1st Commit
    Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        FindObjectOfType<AudioManager>().Play("Background");
+    }
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 force = new Vector3(moveHorizontal, 0f, moveVertical);
+        
+        rb.AddForce(force * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            FindObjectOfType<AudioManager>().Play("Playermove");
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = "You Win";
+        }
+    }
+}
